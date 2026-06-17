89b866bd11a7d3708334631eb814dcdf5273818b
Julio-Machado-Gomes <julio@zarzurgarantidora.com.br>
2026-06-17 04:36:45 +0000

    Analytics: adiciona Microsoft Clarity (heatmaps + gravacao de sessao)
    Snippet instalado no <head> da home, das 4 LPs e do template do blog,
    junto do GA4 e do Meta Pixel. Project ID x8a0j2gmdd. Carregamento assincrono.

--- scripts/build-blog.js
+++ scripts/build-blog.js
@@ -51,6 +51,14 @@ ${jsonld ? `<script type="application/ld+json">${jsonld}</script>` : ""}
   gtag('js', new Date());
   gtag('config', 'G-564QP8QNW4');
 </script>
+<!-- Microsoft Clarity -->
+<script type="text/javascript">
+  (function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "x8a0j2gmdd");
+</script>
 </head>
 <body>`;
 }
